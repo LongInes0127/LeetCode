cdd8781ec12e08fda90405790bb542e608a49fc0
apple <23166321@qq.com>
2023-06-09 11:26:36 +0800

    LeetCode第一题Two Sum。题目链接：https://leetcode.com/problems/two-sum/description/

---
+++ src/main/java/www/atunk/firstweek/monday/TwoSum.java
@@ -0,0 +1,13 @@
+package www.atunk.firstweek.monday;
+
+/**
+ * @description:
+ * @author: 张军
+ * @email: 23166321@qq.com
+ * @date: 2023/6/9 11:23
+ */
+public class TwoSum {
+	public int[] twoSum(int[] nums, int target) {
+		return null;
+	}
+}
